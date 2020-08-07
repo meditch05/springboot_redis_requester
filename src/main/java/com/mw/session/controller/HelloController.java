135c508cc1a0542a16ec294f386030379af09713
P069528 <P069528@SKTP069528PN002.SKT.AD>
2020-08-07 13:35:05 +0900

    1

---
+++ src/main/java/com/mw/session/controller/HelloController.java
@@ -0,0 +1,40 @@
+package com.mw.session.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HelloController {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@GetMapping("hello")
+	public String hello(HttpSession session) {
+		session.setAttribute("hello", "eric");
+		return "hello eric!";
+	}
+	
+    @GetMapping("/request")
+    public String getCookie(HttpSession session) {
+        String sessionKey = session.getId();
+        session.setAttribute("ID", "p069528");
+        logger.info("set userId = {}", "p069528");
+        
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders header = new HttpHeaders();
+        header.add("Cookie", "SESSION=" + sessionKey);
+        HttpEntity<String> requestEntity = new HttpEntity<>(null, header);
+        
+        ResponseEntity<String> cookieValue = restTemplate.exchange("http://localhost:8090/request", HttpMethod.GET, requestEntity, String.class);
+        return "server1_sessionKey : " + session.getId() + "<br>server2_sessionKey : " + cookieValue.getBody();
+    }
+}
